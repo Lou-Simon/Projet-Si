89afb04b8669411d4f4f97bbe4a9813c63e0d34b
mael.bogaer <mael.bogaer@etudiant.univ-brest.fr>
2026-03-03 12:07:56 +0100

    feat: Ajout dockerfile api-scraping et api-artist

--- api-artist/src/main/java/com/controllers/ArtistController.java
+++ api-artist/src/main/java/com/controllers/ArtistController.java
@@ -10,7 +10,6 @@ import java.util.List;
  * Contrôleur REST exposant les endpoints de gestion des artistes.
  */
 @RestController
-@CrossOrigin(origins = "*")
 @RequestMapping("/api/artists")
 @RequiredArgsConstructor
 @CrossOrigin(origins = "*")
